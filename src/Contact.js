1bb4460afec66247e64b60817c05a5693c7e1529
jatin <jatinkalia08@gmail.com>
2023-08-05 11:13:57 +0530

    contact page

--- src/Contact.js
+++ src/Contact.js
@@ -33,9 +33,29 @@ const Contact = () => {
     }
   `;
 
-  return <Wrapper>
-    <h1>Contact</h1>
-  </Wrapper>;
+  return (
+  <Wrapper>
+    <h2 className="common-heading">Contact Us</h2>
+{/* map */}
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3782.261204598636!2d73.91411937410084!3d18.562259067901596!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c147b8b3a3bf%3A0x6f7fdcc8e4d6c77e!2sPhoenix%20Marketcity%20Pune!5e0!3m2!1sen!2sin!4v1691211796346!5m2!1sen!2sin" width="100%" height="400" style={{border:0}}allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+
+    {/* form -method:post because we are ending information*/}
+    <div className="container">
+      <div className="contact-form">
+        {/* contact-inputs class to make it flex */}
+        <form action="https://formspree.io/f/mknloabv" method="POST" className="contact-inputs">
+          {/* we need name and value attribute whenever we need to connect to a backend, autocomplete is off taki previous wali values dikhayi na de */}
+<input type="text" placeholder="username" name="username" required autoComplete="off" />
+<input type="email" placeholder="email" name="Email" required autoComplete="off" />
+<textarea name="Message"  placeholder="Enter your message" cols="30" rows="10" reuired autoComplete="off">
+
+</textarea>
+<input type="submit" value="send" />
+        </form>
+      </div>
+    </div>
+  </Wrapper>
+  );
 };
 
 export default Contact;
