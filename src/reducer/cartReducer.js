e4cca62b4d235dc4485bc6e686d59664c0f3819a
jatin <jatinkalia08@gmail.com>
2023-08-13 19:15:50 +0530

    handling existing items in cart

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -7,20 +7,59 @@ const cartReducer = (state,action) => {
     let {id,color,amount,product}=action.payload;
     // console.log(product);
 
-    let cartProduct;
-    cartProduct={
-      id:id + color, //same prd with diff. color should have diff id, otherwise diff. color produtc will add in 1 prdt.
-      name:product.name,
-      color,
-      amount,
-      image:product.image[0].url,//we get multiple image and we only want 1
-      price:product.price,
-      max:product.stock,
-    }
+    // tackle if a product already exists in the cart
+    let existingProduct=state.cart.find((curItem)=>{
+      return  curItem.id == id + color;  //cart me jo curItem hai kya uski id , jo prd usr cart me add kr rha hai uski id, color se match krti hai
+    })
 
-    return{
-      ...state,
-      cart: [ ...state.cart , cartProduct],//jo pehle data hai uske baad add hoga cartProduct
+
+    if(existingProduct)
+    {
+      // if already exists we have to find it and increse its quanitity
+      let updatedProduct =state.cart.map((curElem)=>{
+
+        if(curElem.id == id + color){
+          let newAmount= curElem.amount + amount;
+
+          // check it should be less than stock
+          if(newAmount >= curElem.max){
+            newAmount=curElem.max;
+          }
+          return {
+            //curelem aisa he rakho bas amount wil be newAmount
+            ...curElem,
+            amount:newAmount,
+          }
+        }else{
+          return {
+            curElem,
+          }
+        }
+        
+      });
+
+      return{
+        ...state,
+        cart: updatedProduct,//jo pehle data hai uske baad add hoga cartProduct
+      }
+
+    }else{
+      //if does not exists we normally add it
+          let cartProduct;
+          cartProduct={
+            id:id + color, //same prd with diff. color should have diff id, otherwise diff. color produtc will add in 1 prdt.
+            name:product.name,
+            color,
+            amount,
+            image:product.image[0].url,//we get multiple image and we only want 1
+            price:product.price,
+            max:product.stock,
+          }
+      
+          return{
+            ...state,
+            cart: [ ...state.cart , cartProduct],//jo pehle data hai uske baad add hoga cartProduct
+          }
     }
   }
 
